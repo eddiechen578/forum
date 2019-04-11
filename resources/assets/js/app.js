8223d6875f6f754a61d4d76d33728644c73e6810
eddiechen <eddiechen578@gmail.com>
2019-04-11 14:59:20 +0800

    done

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('discussion-component', require('./components/DiscussionComponent.vue'));
+// Vue.component('discussion-component', require('./components/DiscussionComponent.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="card">
+   <div class="card">
            @include('partials.discussion-header')
             <div class="card-body">
                 <div class="text-center">
@@ -15,4 +15,32 @@
                 {!! $discussion->content !!}
             </div>
         </div>
+
+    <div class="card my-5">
+        <div class="card-header">
+            Add a reply
+        </div>
+        <div class="card-body">
+          @auth
+            <form action="{{route('replies.store', $discussion->slug)}}" method="post">
+                {{csrf_field()}}
+                <input type="hidden" name="reply" id="reply">
+                <trix-editor input="reply"></trix-editor>
+                <button type="submit" class="btn btn-sm my-2 btn-success">
+                    Create Reply
+                </button>
+            </form>
+          @else
+                <a href="{{route('login')}}" class="btn btn-info">Sign in to add a reply</a>
+          @endauth
+        </div>
+    </div>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.css">
+@endsection
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.js"></script>
 @endsection
